57b704e54d7f261d9073c599b5a140df8da759df
Karim B <mattiakarim@live.it>
2019-06-24 10:49:40 +0200

    Added some styling to the tabs card and tabs card container, now working on the logic to post some new tabs

--- src/components/TabsContainter.jsx
+++ src/components/TabsContainter.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StylesTabsContainer = styled.section`
+    padding: 20px;
+
+    .card-wrapper {
+        position: relative;
+        width: 230px;
+        height: 300px;
+        perspective: 150rem;
+        
+        .side {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border: 3px solid red;
+            border-radius: 10px;
+            transition: all 1s ease-in-out;
+
+            &.back-side {
+                transform: rotateY(180deg);
+            }
+        }
+
+        &:hover .front-side {
+            transform: rotateY(-180deg);
+        }
+
+        &:hover .back-side {
+            transform: rotateY(0);
+        }
+    }
+
+`;
 
 export default function TabsContainer() {
     return (
-        <div>
-            main tab container
-        </div>
+        <StylesTabsContainer>
+            <div className="card-wrapper">
+                <div className="side front-side">
+                    FRONT
+                </div>
+                <div className="side back-side">
+                    BACK
+                </div>
+            </div>
+        </StylesTabsContainer>
     );
 }
